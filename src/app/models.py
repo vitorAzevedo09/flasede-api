6d43b2b915ca9c92a021e9faaa802f4fb858f40d
Vitor Silva <vitor.azevedo009@gmail.com>
2022-04-21 18:28:58 -0300

    Models modifications

--- src/app/models.py
+++ src/app/models.py
@@ -1,33 +1,46 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.sql.expression import text
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Numeric
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from .database import Base
 
 
 class User(Base):
+    ''' Usuario '''
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
+    document = Column(String, nullable=False, unique=True)
+    is_admin = Column(Boolean, default=False)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False,
                         server_default=text('now()'))
 
 
-class Post(Base):
-    __tablename__ = "posts"
-
+class PaymentBook(Base):
+    ''' Carnê '''
+    __tablename__ = "payment_books"
     id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='TRUE', nullable=False)
+    year = Column(Integer, nullable=False)
+    is_payed = Column(Boolean, default=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False,
                         server_default=text('now()'))
-    owner_id = Column(Integer,
+    payer_id = Column(Integer,
                       ForeignKey("users.id", ondelete="CASCADE"),
                       nullable=False)
+    payer = relationship("User")
 
-    owner = relationship("User")
+
+class MonthlyPayment(Base):
+    ''' Mensalidade '''
+    __tablename__ = "monthly_payments"
+    id = Column(Integer, primary_key=True, nullable=False)
+    price = Column(Numeric, primary_key=True, nullable=False)
+    payment_book_id = Column(Integer,
+                             ForeignKey("payment_books.id",
+                                        ondelete="CASCADE"),
+                             nullable=False)
+    payment_book = relationship("PaymentBook")
